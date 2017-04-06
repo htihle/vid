521774c411e428b6c01519b56ac25bd5a3a2f5a4
havard <htihle@gmail.com>
2017-04-06 15:04:54 +0200

    small updates to MCMC.py

--- MCMC.py
+++ MCMC.py
@@ -1,17 +1,31 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import ConfigParser
+import scipy.interpolate as interpolate
+import os
 
 import emcee
 import corner
 
 import VID
+import tools
 
-# This file needs to be updated and improved.
+my_vid = VID.VoxelIntensityDistribution()
+
+Lco = tools.load_cita_lums()
+
+bin_edges, bin_centers, bin_spacings = tools.log_bins(0.9e0, 1.1e8, 200)
+
+phi_hist = np.histogram(Lco, bins=bin_edges)[0]
+
+n_vox = 128*128*1000
+x, y, z = my_vid.get_grid(128)
+volume = (x[-1] - x[0]) * (y[-1] - y[0]) * (z[-1] - z[0])
+
+lum_func = interpolate.interp1d(bin_centers, phi_hist / (bin_spacings * volume))
+
 
 def my_lnprob(par, dtemp_times_n_vox, vid, data):
-    return -np.sum((vid.calculate_vid(par, x)[np.where(data > 0)] * dtemp_times_n_vox - data[
-        np.where(data > 0)]) ** 2 / (2 * data[np.where(data > 0)]))
+    return -np.sum((vid.calculate_vid(lum_func=lum_func, parameters=par, temp_array=x, check_normalization=True)[np.where(data > 0)]
+                    * dtemp_times_n_vox - data[np.where(data > 0)]) ** 2 / (2 * data[np.where(data > 0)]))
 
 
 def do_mcmc_sampling(lnprob, vid, parameters, data, temp_range, n_vox, n_walkers, n_samples, n_burn_in=None, threads=1, autosave=True):
@@ -27,7 +41,7 @@ def do_mcmc_sampling(lnprob, vid, parameters, data, temp_range, n_vox, n_walkers
     ndim = len(parameters)
     p0 = np.array([parameters * (1 + 0.05 * np.random.randn(ndim)) for i in xrange(n_walkers)])
 
-    sampler = emcee.EnsembleSampler(n_walkers, ndim, lnprob, threads=threads,args=(dtemp_times_n_vox,vid,data))
+    sampler = emcee.EnsembleSampler(n_walkers, ndim, lnprob, threads=threads, args=(dtemp_times_n_vox, vid, data))
 
     # burn-in.
     pos, prob, state = sampler.run_mcmc(p0, n_burn_in)
@@ -41,50 +55,30 @@ def do_mcmc_sampling(lnprob, vid, parameters, data, temp_range, n_vox, n_walkers
         values[i] = corner.quantile(sampler.flatchain[:, i], [0.5])
     print values
 
-    print("Mean acceptance fraction:", np.mean(sampler.acceptance_fraction))
+    print "Mean acceptance fraction:", np.mean(sampler.acceptance_fraction)
     if autosave:
+        if not os.path.isdir('autosaves/'):
+            os.makedirs('autosaves/')
         np.save('autosaves/values' + time_string, values)
         np.save('autosaves/samples' + time_string + 'samp' + str(n_samples * n_walkers) + comment, sampler.flatchain)
-    fig = corner.corner(sampler.flatchain,
-                        labels=[r"$\phi_* / 10^{-10}$", r"$L_* / 10^{6}$ ", r"$\alpha$", r"$L_{min} / 10^{2}$ ",
-                                r"$\sigma_G$"], show_titles=True
-                        , levels=(1 - np.exp(-0.5 * np.arange(1, 2.1, 1))))
-    fig.savefig('ConstraintsFromVID' + comment + '.pdf')
-    plt.show()
-
-
-    plt.loglog(x, PofT / dtemp_times_n_vox, x, vid.calculate_vid(values, x))
-    plt.show()
-
-    print "Autocorrelation time:", sampler.get_autocorr_time(c=2)
+    try:
+        print "Autocorrelation time:", sampler.get_autocorr_time(c=2)
+    except emcee.autocorr.AutocorrError:
+        print "Chain is to short to calculate autocorrelation time"
+        pass
     return 0
 
-
-def full_phi(luminosity, fiducial_values, fiducial_units):
-    fid_val = fiducial_units * fiducial_values
-    return fid_val[0] * (luminosity / fid_val[1]) ** fid_val[2] * np.exp(
-        -luminosity / fid_val[1] - fid_val[3] / luminosity)
-
-config = ConfigParser.ConfigParser()
-config.read('parameters.ini')
-
-fid_val = [7.76, 1.34, -1.52, 5.17, 0.79]
-fid_units = [1e-10, 1e6, 1, 1e2, 1]
-
-myVID = VID.VoxelIntensityDistribution(full_phi, fid_val, fid_units, config)
+par = [1.0]
 
 n = 100
 
-binsList = np.logspace(np.log10(5e-9), -4, n + 1)
-box = np.load('tcube.npz')
+binsList = np.logspace(np.log10(1e-8), -4, n + 1)
+box = np.load('cubes/cita_cube.npz')
 flatBox = box.f.t.flatten()
 
-dT = np.zeros(n)
-x = np.zeros(n)
-for i in range(n):
-    dT[i] = binsList[i + 1] - binsList[i]
-    x[i] = (binsList[i + 1] + binsList[i]) / 2.0
+dT = binsList[1:] - binsList[:-1]
+x = (binsList[1:] + binsList[:-1]) / 2.0
 
 PofT = np.histogram((flatBox + 1e-12) * 1e-6, bins=binsList)[0]
 
-do_mcmc_sampling(my_lnprob, myVID, np.array(fid_val), PofT, binsList, 400 * 25 * 25, 10, 5, threads=4)
+do_mcmc_sampling(my_lnprob, my_vid, np.array(par), PofT, binsList, 1000 * 25 * 25, 4, 100, threads=4)
