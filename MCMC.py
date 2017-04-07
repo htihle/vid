289277edebd26788675cf21cc753f02bbe36e764
havard <htihle@gmail.com>
2017-04-07 13:20:39 +0200

    fixes some MCMC.py

--- MCMC.py
+++ MCMC.py
@@ -10,22 +10,22 @@ import tools
 
 my_vid = VID.VoxelIntensityDistribution()
 
-Lco = tools.load_cita_lums()
-
-bin_edges, bin_centers, bin_spacings = tools.log_bins(0.9e0, 1.1e8, 200)
-
-phi_hist = np.histogram(Lco, bins=bin_edges)[0]
-
-n_vox = 128*128*1000
-x, y, z = my_vid.get_grid(128)
-volume = (x[-1] - x[0]) * (y[-1] - y[0]) * (z[-1] - z[0])
-
-lum_func = interpolate.interp1d(bin_centers, phi_hist / (bin_spacings * volume))
+# Lco = tools.load_cita_lums()
+#
+# bin_edges, bin_centers, bin_spacings = tools.log_bins(0.9e0, 1.1e8, 200)
+#
+# phi_hist = np.histogram(Lco, bins=bin_edges)[0]
+#
+# n_vox = 128*128*1000
+# x, y, z = my_vid.get_grid(128)
+# volume = (x[-1] - x[0]) * (y[-1] - y[0]) * (z[-1] - z[0])
+#
+# lum_func = interpolate.interp1d(bin_centers, phi_hist / (bin_spacings * volume))
 
 
 def my_lnprob(par, dtemp_times_n_vox, vid, data):
-    return -np.sum((vid.calculate_vid(lum_func=lum_func, parameters=par, temp_array=x, check_normalization=True)[np.where(data > 0)]
-                    * dtemp_times_n_vox - data[np.where(data > 0)]) ** 2 / (2 * data[np.where(data > 0)]))
+    return -np.sum((vid.calculate_vid(parameters=par, temp_array=x)[np.where(data > 0)]
+                    - data[np.where(data > 0)]) ** 2 / (2 * data[np.where(data > 0)]))
 
 
 def do_mcmc_sampling(lnprob, vid, parameters, data, temp_range, n_vox, n_walkers, n_samples, n_burn_in=None, threads=1, autosave=True):
@@ -68,17 +68,12 @@ def do_mcmc_sampling(lnprob, vid, parameters, data, temp_range, n_vox, n_walkers
         pass
     return 0
 
-par = [1.0]
-
+# par = [1.0]
+best_fit_regular = [10.13348524e-10,   1.07747679e6,  -1.5833037,   13.50122216e2,   0.03816793]
 n = 100
 
 binsList = np.logspace(np.log10(1e-8), -4, n + 1)
-box = np.load('cubes/cita_cube.npz')
-flatBox = box.f.t.flatten()
 
-dT = binsList[1:] - binsList[:-1]
-x = (binsList[1:] + binsList[:-1]) / 2.0
+PofT, x = tools.vid_from_cube('cubes/cita_cube.npz', temp_range=binsList, add_noise=True, noise_temp=15.3)
 
-PofT = np.histogram((flatBox + 1e-12) * 1e-6, bins=binsList)[0]
-
-do_mcmc_sampling(my_lnprob, my_vid, np.array(par), PofT, binsList, 1000 * 25 * 25, 4, 100, threads=4)
+do_mcmc_sampling(my_lnprob, my_vid, np.array(best_fit_regular), PofT, binsList, 1000 * 25 * 25, 4, 5, threads=4)
