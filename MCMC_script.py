7068120c7a1234f99be937b08bb98dc05ab23398
havard <htihle@gmail.com>
2017-06-06 16:48:42 +0200

    added choise of fixed or non-fixed lnprob

--- MCMC_script.py
+++ MCMC_script.py
@@ -13,7 +13,7 @@ comm = MPI.COMM_WORLD
 my_rank, size = (comm.Get_rank(), comm.Get_size())
 
 
-def my_lnprob(par, n_vox, vid, data):
+def my_lnprob_fixedcut(par, n_vox, vid, data):
     if (par[-1] < 0) or (par[0] < 0) or (par[1] < 2e4) or (par[2] < -3.5):
         return - np.inf
     parm = np.zeros(5)
@@ -31,9 +31,27 @@ def my_lnprob(par, n_vox, vid, data):
     warnings.filterwarnings('default')
     return return_value
 
+
+def my_lnprob(par, n_vox, vid, data):
+    if (par[-1] < 0) or (par[0] < 0) or (par[1] < 2e4) or (par[2] < -3.5) or (par[3] < 1e1):
+        return - np.inf
+    B_i = vid.calculate_vid(parameters=par, temp_array=bin_edges, subtract_mean_temp=True, bin_counts=True) * n_vox
+    warnings.filterwarnings('error')
+    try:
+        return_value = -np.sum((B_i - data) ** 2 / (2 * B_i))
+    except RuntimeWarning:
+        print "RuntimeWarning caught, returning - np.inf"
+        print "parm = ", par
+        return_value = - np.inf
+    warnings.filterwarnings('default')
+    return return_value
+
 my_vid = VID.VoxelIntensityDistribution()
 
+best_fit = [10.13348524e-10,   1.07747679e6,  -1.5833037,   13.50122216e2,   0.03816793]
 best_fit_fixedcut = [10.13348524e-10, 1.07747679e6, -1.5833037, 0.03816793]
+
+
 n = 100
 
 bin_edges, bin_centers, bin_spacings = tools.log_bins(1e-5, 1e-4, n)
@@ -53,5 +71,8 @@ for i in my_indices:
     data, x = tools.vid_from_cube('cubes/cita_cube_' + str(i) + '.npz', temp_range=bin_edges, add_noise=True,
                                   noise_temp=15.3, subtract_mean=True, bin_count=True)
 
-    MCMC.do_mcmc_sampling(my_lnprob, np.array(best_fit_fixedcut), (n_vox, my_vid, data), 10, 20, threads=4,
+    # MCMC.do_mcmc_sampling(my_lnprob_fixedcut, np.array(best_fit_fixedcut), (n_vox, my_vid, data), 10, 20, threads=4,
+    #                       comment='cube' + str(i))
+
+    MCMC.do_mcmc_sampling(my_lnprob, np.array(best_fit), (n_vox, my_vid, data), 10, 20, threads=4,
                           comment='cube' + str(i))
